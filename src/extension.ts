bd65e7c3b16555bbf13c8af6a5286dabad70778a
Mutsuha Asada <me@momee.mt>
2024-01-16 23:33:50 +0900

    implemented a simple client

---
+++ src/extension.ts
@@ -0,0 +1,36 @@
+import { ExtensionContext, window } from 'vscode'
+import { LanguageClient } from 'vscode-languageclient/node'
+
+let client: LanguageClient
+
+export const activate = (_: ExtensionContext) => {
+    try {
+        const serverOptions = {
+            command: "brack --language-server"
+        }
+        const clientOptions = {
+            documentSelector: [
+                {
+                    scheme: 'file',
+                    language: 'brack'
+                }
+            ],
+        }
+        client = new LanguageClient(
+            "brack-language-server",
+            "Brack Language Server",
+            serverOptions,
+            clientOptions
+        )
+        client.start()
+    } catch (error) {
+        window.showErrorMessage("brack-language-server could not be started.")   
+    }
+}
+
+export const deactivate = (): Thenable<void> | undefined => {
+    if (!client) {
+        return undefined
+    }
+    return client.stop()
+}
